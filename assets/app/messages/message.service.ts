a26dff044f7c94656b89c7ec5fad0c2d814315ed
kreafan <legendarnosti@gmail.com>
2017-05-01 18:42:10 +0300

    changed link

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -20,7 +20,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.post('https://angular2-2deployment.herokuapp.com/message' + token, body, {headers: headers})
+        return this.http.post('https://scircles.herokuapp.com/message' + token, body, {headers: headers})
             .map((response: Response) => {
                 const result = response.json();
                 const message = new Message(
@@ -38,7 +38,7 @@ export class MessageService {
     }
 
     getMessages() {
-        return this.http.get('https://angular2-2deployment.herokuapp.com/message')
+        return this.http.get('https://scircles.herokuapp.com/message')
             .map((response: Response) => {
                 const messages = response.json().obj;
                 let transformedMessages : Message[] = [];
@@ -69,7 +69,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.patch('https://angular2-2deployment.herokuapp.com/message/' + message.messageId + token, body, {headers: headers})
+        return this.http.patch('https://scircles.herokuapp.com/message/' + message.messageId + token, body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
@@ -82,7 +82,7 @@ export class MessageService {
         const token = localStorage.getItem('token')
             ? '?token=' + localStorage.getItem('token')
             : '';
-        return this.http.delete('https://angular2-2deployment.herokuapp.com/message/' + message.messageId + token)
+        return this.http.delete('https://scircles.herokuapp.com/message/' + message.messageId + token)
             .map((response: Response) => response.json())
             .catch((error: Response) => {
                 this.errorService.handleError(error.json());
